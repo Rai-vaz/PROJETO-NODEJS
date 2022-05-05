e3f6956576ab991200630186ae8007af5fc08e6a
Rai-vaz <ray.vaz04@gmail.com>
2022-05-05 15:28:11 -0300

    EDIT

--- src/server.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const routes = require("./routes");
-const app = express();
-app.use(routes);
-const port = 3333;
-app.listen(port,()=>{console.log(`backend acessado na porta ${port}`)})
